bcd03e8cd5087056b897d63f00981dbaa72ccac0
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-01 21:12:51 +0200

    title

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,10 +3,17 @@ import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import ProtoType from "prop-types";
 
-function Navbar() {
+function Navbar({ title }) {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-      <div className="container mx-auto">Navbar</div>
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 }
