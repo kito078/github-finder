8ed390719ebe3c6b8cb9c6607ca54f99d7d597c9
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-02 06:09:48 +0200

    data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,30 +2,10 @@ import React, { useEffect } from "react";
 import UserResults from "../components/users/UserResults";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-    console.log(data);
-  };
   return (
     <div>
       <h1 className="text-6xl">
-        <UserResults />>
+        <UserResults />
       </h1>
     </div>
   );
