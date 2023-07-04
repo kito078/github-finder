1eed7d1113e1b359d90c4febb3a4aa9cf4b1b5a8
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-04 13:07:13 +0200

    profile

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect } from "react";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Spinner from "../components/layout/Spiner";
 
 function User() {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
 
   const params = useParams();
 
@@ -12,7 +15,53 @@ function User() {
     // getUserRepos(params.login);
   }, []);
 
-  return <div>{user.login}</div>;
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  console.log(user);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div className="mb-4">
+          <Link to="/" className="btn btn-ghost">
+            Back To Search
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+              <div className="card-body justify-end">
+                <h2 className="caaard-title mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default User;
